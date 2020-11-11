a7082913e0c2fc443ac738c7e9bc447f4e01cbf9
ccaprini <ccaprini@deloitte.nl>
2020-11-11 23:09:39 +0100

    Add testing script

---
+++ start_clening.py
@@ -0,0 +1,19 @@
+import serial
+import time
+ 
+# Open a serial connection to Roomba
+ser = serial.Serial(port='/dev/serial0', baudrate=115200)
+ 
+# Assuming the robot is awake, start safe mode. Note that 0x80 in hexadecimal corresponds to 128.
+ser.write('\x80')
+ 
+time.sleep(.1)
+ 
+# Start cleaning - 135
+ser.write('\x87')
+ 
+# Stop (back to off mode) - 135
+ser.write('\x87')
+ 
+# Close the serial port; we're done for now.
+ser.close()
