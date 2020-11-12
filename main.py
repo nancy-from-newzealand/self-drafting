832909d2eb325cece3dc757b5579fb34862494cc
taffy90 <tboudewijns@gmail.com>
2020-11-12 23:27:32 +0100

    Set functions for steering and throttling

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 import serial
 import time
-from remote_control_config import{
+from remote_control_config import(
     steer,
-}
-def main()
+)
+def main():
  
-# Open a serial connection to Roomba
-ser = serial.Serial(port='/dev/serial0', baudrate=115200)
- 
-# Assuming the robot is awake, start passive mode. Note that 0x80 in hexadecimal corresponds to 128.
-ser.write('\x80')
-time.sleep(.1)
+    # Open a serial connection to Roomba
+    ser = serial.Serial(port='/dev/serial0', baudrate=115200)
+     
+    # Assuming the robot is awake, start passive mode. Note that 0x80 in hexadecimal corresponds to 128.
+    ser.write('\x80')
+    time.sleep(.1)
 
-# Assuming the robot is awake, start safe mode. Note that 0x82 in hexadecimal corresponds to 130.
-ser.write('\x82')
-time.sleep(.1)
+    # Assuming the robot is awake, start safe mode. Note that 0x82 in hexadecimal corresponds to 130.
+    ser.write('\x82')
+    time.sleep(.1)
 
-direction = "left"
+    #direction = "straight-backwards"
+    #direction = "straight-forwards"
+    #direction = "left-forwards"
+    #direction = "right-forwards"
+    #direction = "right-backwards"
+    direction = "left-backwards"
 
-steer(direction, ser)
+    steer(direction, ser)
 
-time.sleep(3)
-#stop: 173
-ser.write('\xAD')
+    time.sleep(1)
+    #stop: 173
+    ser.write('\xAD')
 
-# Close the serial port; we're done for now.
-ser.close()
+    # Close the serial port; we're done for now.
+    ser.close()
 
-return
+    return
 
 main()
--- remote_control_config.py
+++ remote_control_config.py
@@ -3,7 +3,28 @@ import time
 
 def steer (direction, ser):
 
-    if direction == "left":
+    if direction == "straight-forwards":
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+        # 137
+        ser.write('\x89')
+        time.sleep(.1)
+        # 255
+        ser.write('\x00')
+        time.sleep(.1)
+        # 56
+        ser.write('\x64')
+        time.sleep(.1)
+        # 1
+        ser.write('\x7F')
+        time.sleep(.1)
+        # 244
+        ser.write('\xFF')
+        status = 200
+
+    if direction == "straight-backwards":
     #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
     # #you would send the serial byte sequence [137] [255] [56] [1] [244].
     # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
@@ -15,13 +36,97 @@ def steer (direction, ser):
         ser.write('\xFF')
         time.sleep(.1)
         # 56
-        ser.write('\x38')
+        ser.write('\x9C')
         time.sleep(.1)
         # 1
+        ser.write('\x7F')
+        time.sleep(.1)
+        # 244
+        ser.write('\xFF')
+        status = 200
+
+    if direction == "left-forwards":
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+        # 137
+        ser.write('\x89')
+        time.sleep(.1)
+        # 0064 ---> VELOCITY 100
+        ser.write('\x00')
+        time.sleep(.1)
+        # 56
+        ser.write('\x64')
+        time.sleep(.1)
+        # 012C ---> RADIUS 300
         ser.write('\x01')
         time.sleep(.1)
         # 244
-        ser.write('\xF4')
+        ser.write('\x2C')
+        status = 200
+    
+    if direction == "right-forwards":
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+        # 137
+        ser.write('\x89')
+        time.sleep(.1)
+        # 0064 ---> VELOCITY 100
+        ser.write('\x00')
+        time.sleep(.1)
+        # 56
+        ser.write('\x64')
+        time.sleep(.1)
+        # FED4 ---> RADIUS -300
+        ser.write('\xFE')
+        time.sleep(.1)
+        # 244
+        ser.write('\xD4')
+        status = 200
+
+    if direction == "right-backwards":
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+        # 137
+        ser.write('\x89')
+        time.sleep(.1)
+        # 0064 ---> VELOCITY 100
+        ser.write('\xFF')
+        time.sleep(.1)
+        # 56
+        ser.write('\x9C')
+        time.sleep(.1)
+        # 012C ---> RADIUS 300
+        ser.write('\xFE')
+        time.sleep(.1)
+        # 244
+        ser.write('\xD4')
+        status = 200
+
+    if direction == "left-backwards":
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+        # 137
+        ser.write('\x89')
+        time.sleep(.1)
+        # 0064 ---> VELOCITY -100
+        ser.write('\xFF')
+        time.sleep(.1)
+        # 56
+        ser.write('\x9C')
+        time.sleep(.1)
+        # 012C ---> RADIUS 300
+        ser.write('\x01')
+        time.sleep(.1)
+        # 244
+        ser.write('\x2C')
         status = 200
 
     return status
