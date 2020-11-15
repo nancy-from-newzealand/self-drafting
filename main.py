bce76f69e48b9373356d491592116c6da640029c
taffy90 <tboudewijns@gmail.com>
2020-11-15 04:02:42 +0100

    Keyboard works on pingu

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import serial
 import time
 from remote_control_config import(
     steer,
-    straight_left,
+    forward,
+    backward,
+    forward_left,
+    forward_right,
+    backward_left,
+    backward_right,
+    stop
+
 )
 from keyboard_control import(
     init,
@@ -15,11 +22,11 @@ def main():
      
     # Assuming the robot is awake, start passive mode. Note that 0x80 in hexadecimal corresponds to 128.
     ser.write('\x80')
-    time.sleep(.1)
+    #time.sleep(.1)
 
     # Assuming the robot is awake, start safe mode. Note that 0x82 in hexadecimal corresponds to 130.
     ser.write('\x82')
-    time.sleep(.1)
+    #time.sleep(.1)
 
     #direction = "straight-backwards"
     #direction = "straight-forwards"
@@ -27,17 +34,38 @@ def main():
     #direction = "right-forwards"
     #direction = "right-backwards"
     #direction = "left-backwards"
+    
     if getKey('UP') and getKey('LEFT'):
-        straight_left(ser)
-        print('straight-left')
+        forward_left(ser)
+        print('forward_left')
+    if getKey('UP') and getKey('RIGHT'):
+        forward_right(ser)
+        print('forward_right')
+    if getKey('UP') and not(getKey('RIGHT')) and not(getKey('LEFT')):
+        forward(ser)
+        print('forward')
+
+    if getKey('DOWN') and getKey('LEFT'):
+        backward_left(ser)
+        print('backward_left')
+    if getKey('DOWN') and getKey('RIGHT'):
+        backward_right(ser)
+        print('backward_right')
+    if getKey('DOWN') and not(getKey('RIGHT')) and not(getKey('LEFT')):
+        backward(ser)
+        print('backward')
+    if getKey('SPACE'):
+        stop(ser)
+        print('stop')
+
     #steer(direction, ser)
 
-    time.sleep(1)
+    time.sleep(.1)
     #stop: 173
-    ser.write('\xAD')
+    #ser.write('\xAD')
 
     # Close the serial port; we're done for now.
-    ser.close()
+    #ser.close()
 
     return
 
--- remote_control_config.py
+++ remote_control_config.py
@@ -132,24 +132,139 @@ def steer (direction, ser):
     return status
 
 
-
-def straight_left(ser):
- #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+def forward(ser):
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
     # #you would send the serial byte sequence [137] [255] [56] [1] [244].
     # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
 
-        # 137
-        ser.write('\x89')
-        time.sleep(.1)
-        # 0064 ---> VELOCITY 100
-        ser.write('\x00')
-        time.sleep(.1)
-        # 56
-        ser.write('\x64')
-        time.sleep(.1)
-        # 012C ---> RADIUS 300
-        ser.write('\x01')
-        time.sleep(.1)
-        # 244
-        ser.write('\x2C')
-        return 200
+    # 137
+    ser.write('\x89')
+    time.sleep(.1)
+    # 255
+    ser.write('\x00')
+    time.sleep(.1)
+    # 56
+    ser.write('\x64')
+    time.sleep(.1)
+    # 1
+    ser.write('\x7F')
+    time.sleep(.1)
+    # 244
+    ser.write('\xFF')
+
+    return 200
+
+def forward_left(ser):
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+    # 137
+    ser.write('\x89')
+    time.sleep(.1)
+    # 0064 ---> VELOCITY 100
+    ser.write('\x00')
+    time.sleep(.1)
+    # 56
+    ser.write('\x64')
+    time.sleep(.1)
+    # 012C ---> RADIUS 300
+    ser.write('\x01')
+    time.sleep(.1)
+    # 244
+    ser.write('\x2C')
+    return 200
+
+def forward_right(ser):
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+    # 137
+    ser.write('\x89')
+    time.sleep(.1)
+    # 0064 ---> VELOCITY 100
+    ser.write('\x00')
+    time.sleep(.1)
+    # 56
+    ser.write('\x64')
+    time.sleep(.1)
+    # FED4 ---> RADIUS -300
+    ser.write('\xFE')
+    time.sleep(.1)
+    # 244
+    ser.write('\xD4')
+
+    return 200
+
+def backward(ser):
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+    # 137
+    ser.write('\x89')
+    #time.sleep(.1)
+    # 255
+    ser.write('\xFF')
+    #time.sleep(.1)
+    # 56
+    ser.write('\x9C')
+    #time.sleep(.1)
+    # 1
+    ser.write('\x7F')
+    #time.sleep(.1)
+    # 244
+    ser.write('\xFF')
+    
+    return 200
+
+def backward_left(ser):
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+    # 137
+    ser.write('\x89')
+    time.sleep(.1)
+    # 0064 ---> VELOCITY -100
+    ser.write('\xFF')
+    time.sleep(.1)
+    # 56
+    ser.write('\x9C')
+    time.sleep(.1)
+    # 012C ---> RADIUS 300
+    ser.write('\x01')
+    time.sleep(.1)
+    # 244
+    ser.write('\x2C')
+
+    return 200
+
+def backward_right(ser):
+    #To drive in reverse at a velocity of -200 mm/s while turning at a radius of 500mm, 
+    # #you would send the serial byte sequence [137] [255] [56] [1] [244].
+    # Velocity = -200 = hex FF38 = [hex FF] [hex 38] = [255] [56] Radius = 500 = hex 01F4 = [hex 01] [hex F4] = [1] [244]
+
+    # 137
+    ser.write('\x89')
+    time.sleep(.1)
+    # 0064 ---> VELOCITY 100
+    ser.write('\xFF')
+    time.sleep(.1)
+    # 56
+    ser.write('\x9C')
+    time.sleep(.1)
+    # 012C ---> RADIUS 300
+    ser.write('\xFE')
+    time.sleep(.1)
+    # 244
+    ser.write('\xD4')
+
+    return 200
+
+
+def stop(ser):
+    #stop: 173
+    ser.write('\xAD')
+    return 200
