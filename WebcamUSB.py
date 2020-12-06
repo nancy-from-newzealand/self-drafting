5947d66433218e940392b38c5572c767b9204f33
taffy90 <tboudewijns@gmail.com>
2020-12-06 18:41:40 +0100

    Acquire live data from cam

--- WebcamUSB.py
+++ WebcamUSB.py
@@ -1,19 +1,24 @@
 import cv2
  
 cap = cv2.VideoCapture(-1)
+#cap.release()
 print(cap)
  
 def getImg(display= False,size=[480,240]):
     _, img = cap.read()
     print(img)
 
-    #img = cv2.resize(img,(size[0],size[1]))
+    img = cv2.resize(img,(size[0],size[1]))
     if display:
         cv2.imshow('IMG',img)
+        # cv2.waitKey()
     return img, cap
  
 if __name__ == '__main__':
     while True:
         img, cap = getImg(True)
-        #cap.release()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
         
---
+++ test.py
@@ -0,0 +1,5 @@
+import cv2
+ 
+cap = cv2.VideoCapture(-1)
+cap.release()
+print(cap)
